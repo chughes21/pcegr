f78cbfdc25a770d7139ae0e9317661a3a251a1b3
Conor Hughes <hughesco@tcd.ie>
2022-10-07 16:09:24 +0100

    Added other functions

---
+++ R/other_functions.R
@@ -0,0 +1,25 @@
+#' The time minimiser
+#'
+#' A function that enforces a minimum value of time for an observed time.
+#'
+#' @param data A data set where the last column is the observed time vector.
+#' @param t_min A numeric minimum time value.
+#'
+#' @return A data set where the observed times now have a minimum value.
+#' @export
+#'
+#' @examples
+#' n<-10
+#' t<-runif(n)
+#' y<-rpois(n,2*t)
+#' data<-data.frame(y=y,t=t)
+#' data
+#' data.min<-time_minimiser(data,t_min=0.5)
+#' data.min
+time_minimiser<-function(data,t_min){
+  n<-dim(data)[2]
+  t<-data[,n]
+  ind<-which(t<t_min)
+  data[ind,n]<-t_min
+  return(data)
+}
