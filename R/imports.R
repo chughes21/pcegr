045ef41bdcf4906bd5d2a9ff36c8af9425ebfcba
Conor Hughes <hughesco@tcd.ie>
2022-10-17 13:08:17 +0100

    Updated Namespace, added if_else import

--- R/imports.R
+++ R/imports.R
@@ -1,7 +1,7 @@
 #' @importFrom bayestestR distribution_gamma distribution_beta hdi
 #' @importFrom prodlim row.match
 #' @importFrom tidyr expand_grid
-#' @importFrom dplyr mutate
+#' @importFrom dplyr mutate if_else
 #' @importFrom reshape2 melt
 #' @import ggplot2
 #' @import ggrepel
