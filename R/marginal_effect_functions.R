98d4a74e3eb7059c011d53d2d48c68d68a307d6a
Conor Hughes <hughesco@tcd.ie>
2023-01-27 12:58:47 +0000

    Updated marginal effects plot

--- R/marginal_effect_functions.R
+++ R/marginal_effect_functions.R
@@ -82,7 +82,7 @@ marginal_effect<-function(data,mod,input_variable = c(),rel_output=0,max_per_plo
 
   output<-pcegr:::parameter_extractor(stage.struct,posterior,output_variable,poisson_response,remove_risk_free) #used to have n1 instead of output_variable, but now I think they're the same
 
-  cv<-output_variable-1 #the number of covariates that will be used for this
+  cv<-output_variable-1-1*zip*(rel_output==0) #the number of covariates that will be used for this
   cv_ind<-1:cv
 
   if(!poisson_response){
