fbc6a32c321c5e16926da154a238cd8309699ee9
Conor Hughes <hughesco@tcd.ie>
2023-07-26 18:58:25 +0100

    Added loglik function, but it's not ready

---
+++ R/loglikelihood_function_NOT_READY.R
@@ -0,0 +1,73 @@
+#Currently not ready due to missing the data dependent part of a Poisson
+#Also could fold into the VIF
+
+#' Log Likelihood Calculator
+#'
+#' Calculates the log likelihood and BIC for a StagedTree object.
+#'
+#' @param mod A StagedTree object.
+#'
+#' @return A list containing the log likelihood, number of stages and BIC
+#' @export
+#'
+#' @examples
+#' mod<-pceg(knee_pain_obs,2,TRUE,TRUE)
+#' loglik(mod)
+loglik<-function(mod){
+
+  data.sum<-mod$data.summary
+  posterior<-mod$posterior.expectation
+
+  poisson.response<-mod$event.tree$poisson.response
+
+  stages<-0
+  result<-0
+  n<-0
+
+  for(i in 1:length(posterior)){
+
+    if(poisson.response & i==length(posterior)){
+      poisson<-TRUE
+    }else{poisson<-FALSE}
+
+    d<-data.sum[[i]]
+    post<-posterior[[i]]
+
+    ind<-which(!is.na(d[,1]))
+
+    d<-d[ind,]
+    if(poisson){
+     post<-post[ind]
+    }else{
+    post<-post[ind,]
+    }
+
+    for(j in 1:length(ind)){
+     if(poisson){
+
+       stop("Function doesn't work for Poissons - yet")
+
+
+       }else{
+      if(length(ind)>1){
+
+        temp<-sum(d[j,]*log(post[j,]))
+
+       }else{
+
+        temp<-sum(d*log(post))
+
+       }
+      }
+    result<-result+temp
+    stages<-stages+1
+
+    }
+
+    n<-n+sum(d)
+
+  }
+
+  return(list(loglik=result,stages=stages,BIC=stages*log(n)-2*result))
+
+}
